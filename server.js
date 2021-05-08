a394e999eb11de642d255a1e03adf6bfe0af0d18
oconda90 <70242240+oconda90@users.noreply.github.com>
2021-05-07 20:50:01 -0700

    user can save and submit post

--- server.js
+++ server.js
@@ -1,11 +1,40 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const hbs = require('express-handlebars');
+const path = require('path')
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Add after body parser initialization!
+app.use(expressValidator());
+
+app.engine('hbs', hbs({
+  layoutsDir: __dirname + '/views/layouts',
+  defaultLayout: 'index',
+  extname: 'hbs'
+}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+    res.render('main', { layout: 'index' });
+});
+
+app.get("/posts/new", (req, res) => {
+  res.render("posts-new.hbs")
 })
+
+
+
+require('./controllers/posts.js')(app);
+// Set db
+require('./data/reddit-db');
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`Example app listening at http://localhost:${port}`)
+}) 
